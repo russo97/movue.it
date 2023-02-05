10657279ce7e8767f45fc3b76c7747c07801dcb5
Wanderson Silva <wandersonsilva768@gmail.com>
2023-02-05 02:24:49 -0300

    feat: estrutura e estilização do Profile

--- components/molecules/Profile.vue
+++ components/molecules/Profile.vue
@@ -1,13 +1,40 @@
 <template>
 	<div>
-		profile
+		<figure class="mr-6 w-20 h-20 rounded-full overflow-hidden">
+			<img
+				src="https://github.com/russo97.png"
+				alt="GitHub profile pic"
+			/>
+		</figure>
+
+		<div class="flex flex-col w-full">
+			<strong class="text-title text-2xl">
+				Wanderson Silva
+			</strong>
+
+			<p class="flex items-center">
+				<img
+					src="icons/level.svg"
+					alt="level icon"
+					class="mr-2"
+				/>
+
+				<span class="text-base">
+					Level {{ level }}
+				</span>
+			</p>
+		</div>
 	</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
+import { mapState } from 'vuex';
+
 export default Vue.extend({
 	name: 'Profile',
+
+	computed: mapState('Challenges', ['level']),
 });
 </script>
