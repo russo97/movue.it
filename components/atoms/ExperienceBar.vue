1c8f9386380ca31342ae69edacf62936eb3264db
Wanderson Silva <wandersonsilva768@gmail.com>
2023-02-05 00:21:10 -0300

    feat: criado componente que representará a barra de experiência do usuário

---
+++ components/atoms/ExperienceBar.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		i am experience bar
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	name: 'ExperienceBar',
+});
+</script>
+
+<style scoped>
+
+</style>
