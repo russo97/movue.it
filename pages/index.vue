523f9656c11c70407f74e16f637d6dc01f6a6487
Wanderson Silva <wandersonsilva768@gmail.com>
2023-02-07 01:05:39 -0300

    feat: adicionada estrutura e funcionalidades básicas na página inicial

--- pages/index.vue
+++ pages/index.vue
@@ -6,12 +6,42 @@
 			<Profile />
 
 			<CompletedChallenges />
+
+			<CountDown @completed="getNewChallenge" />
+
+			<button
+				v-if="hasCountdownCompleted"
+				disabled
+				class="button completed"
+			>
+				Cycle completed
+			</button>
+
+			<button
+				v-else-if="isCountdownActive"
+				class="button abandon"
+				@click="setCountdownState(false)"
+			>
+				Abandon cycle
+			</button>
+
+			<button
+				v-else
+				class="button start"
+				@click="setCountdownState(true)"
+			>
+				Start a cycle
+			</button>
 		</div>
 	</section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+
+import { mapState, mapMutations } from 'vuex';
+
+import { Mutations as CountdownMutations } from '~/store/Countdown/types';
 
 interface Head {
 	title: string;
@@ -24,6 +54,29 @@ export default Vue.extend({
 		return {
 			title: 'Home | Movue.it',
 		};
+	},
+
+	computed: {
+		...mapState('Countdown', {
+			isCountdownActive: 'isActive',
+			hasCountdownCompleted: 'hasCompleted',
+		}),
+	},
+
+	methods: {
+		...mapMutations({
+			setCountdownIsActive: `Countdown/${CountdownMutations.SET_IS_ACTIVE}`,
+			setCountDownHasCompleted: `Countdown/${CountdownMutations.SET_HAS_COMPLETED}`,
+		}),
+
+		setCountdownState (flag: boolean) {
+			this.setCountDownHasCompleted(false);
+			this.setCountdownIsActive(flag);
+		},
+
+		getNewChallenge () {
+			this.setCountDownHasCompleted(true);
+		},
 	},
 });
 </script>
