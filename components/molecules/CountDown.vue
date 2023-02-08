2dded018162ffc0d3b6768fb1e5bdd528f6c170e
Wanderson Silva <wandersonsilva768@gmail.com>
2023-02-07 23:02:12 -0300

    feat: finalizada exibição e controle do countdown

--- components/molecules/CountDown.vue
+++ components/molecules/CountDown.vue
@@ -1,17 +1,74 @@
 <template>
-	<div>
-		countdown
+	<div class="flex justify-center items-center mt-8 lg:mt-14 text-9xl text-title font-rajdhani">
+		<CountdownDigits :digits="minutes" />
+
+		<span class="bg-background px-2">
+			:
+		</span>
+
+		<CountdownDigits :digits="seconds" />
 	</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
+import { mapState, mapGetters, mapMutations } from 'vuex';
+
+import { Mutations } from '~/store/Countdown/types';
+
+let TIMEOUT_REFERENCE: ReturnType<typeof setTimeout>;
+
 export default Vue.extend({
 	name: 'CountDown',
+
+	computed: {
+		...mapState('Countdown', [
+			'time',
+			'isActive',
+		]),
+
+		...mapGetters('Countdown', [
+			'minutes',
+			'seconds',
+		]),
+	},
+
+	methods: {
+		...mapMutations('Countdown', {
+			setTime: Mutations.SET_TIME,
+			resetTime: Mutations.RESET_TIME,
+		}),
+
+		runCountdown (flag: boolean) {
+			if (this.isActive && flag) {
+				TIMEOUT_REFERENCE = setTimeout(() => {
+					this.setTime(this.time - 1);
+				}, 1000);
+
+				return;
+			}
+
+			clearInterval(TIMEOUT_REFERENCE);
+		},
+	},
+
+	watch: {
+		isActive (newValue: boolean) {
+			this.runCountdown(newValue);
+
+			if (!newValue) {
+				this.resetTime();
+			}
+		},
+
+		time (newValue: number) {
+			if (newValue > 0) {
+				return this.runCountdown(true);
+			}
+
+			this.$emit('completed');
+		},
+	},
 });
 </script>
-
-<style scoped>
-
-</style>
