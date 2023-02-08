16c09531385e08ff0d8529c0237ff4863e44c155
Wanderson Silva <wandersonsilva768@gmail.com>
2023-02-07 22:40:44 -0300

    feat: adicionada função responsável por tratar os digitos a serem exibidos no elemento de countdown

--- utils/index.ts
+++ utils/index.ts
@@ -2,3 +2,5 @@
 export const playAudio = (path: string) => new Audio(path).play();
 
 export const sendNotification = (title: string, options?: object) => new Notification(title, options);
+
+export const splitValue = (value: number, padSize: number = 2) => `${value}`.padStart(padSize, '0').split('');
