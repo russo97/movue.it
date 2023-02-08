8447a5cefef416512146524155a65501a8eb322b
Wanderson Silva <wandersonsilva768@gmail.com>
2023-02-07 22:18:07 -0300

    feat: criação do componente responsável por renderizar a contagem regressiva da aplicação

---
+++ components/atoms/CountdownDigits.vue
@@ -0,0 +1,15 @@
+<template>
+	digit
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	name: 'CountdownDigits',
+});
+</script>
+
+<style scoped>
+
+</style>
