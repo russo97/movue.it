515324cb5abb48159f29b6a09e7e347c18097ac6
Wanderson Silva <wandersonsilva768@gmail.com>
2023-02-09 00:01:58 -0300

    feat: adicionada função getRandomNumber para geração de números randômicos

--- utils/index.ts
+++ utils/index.ts
@@ -4,3 +4,7 @@ export const playAudio = (path: string) => new Audio(path).play();
 export const sendNotification = (title: string, options?: object) => new Notification(title, options);
 
 export const splitValue = (value: number, padSize: number = 2) => `${value}`.padStart(padSize, '0').split('');
+
+export const getRandomNumber = (min: number, max: number) => {
+	return Math.floor(Math.random() * (max - min) + min);
+};
